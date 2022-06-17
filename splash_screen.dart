fa2ab03a76e71812a6fd75ac7aafdcf32ef135a2
C519 <107685849+C519@users.noreply.github.com>
2022-06-17 20:41:22 +0800

    Add files via upload

---
+++ splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:posttest6_1915016121_rhennanurlisa/controller/homepage.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    splashScreenStart();
+    super.initState();
+  }
+
+  splashScreenStart() {
+    var duration = Duration(seconds: 4);
+    return Timer(duration, () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) {
+          return HomePage();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/nanana.png",
+          width: MediaQuery.of(context).size.width / 2,
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
+    );
+  }
+}
